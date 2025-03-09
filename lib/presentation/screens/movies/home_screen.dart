1b14df807088b95249d42e457e850242e5ae68cb
Santiago Velez <macbookpro@MacBook-Pro-de-Santiago.local>
2025-03-09 16:45:09 -0500

    datasources and repositories implementation

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,7 +10,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: Text(Environment.movieDBKey),
+          child: Placeholder()
         ),
     );
   }
